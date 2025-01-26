2fea089a9437d38fa7eef961091ea33b2595e305
GarciaDos <ninogarcia3209@gmail.com>
2025-01-26 10:29:20 +0800

    trying to add audio button

---
+++ app.js
@@ -0,0 +1,7 @@
+const audioButton = document.querySelector('nav-btton');
+const audio = document.querySelector('firetreasure');
+
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play()
+}
