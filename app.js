4bf7669919b0c4c826dd4fe532bd039d737696d7
GarciaDos <ninogarcia3209@gmail.com>
2025-01-26 11:32:10 +0800

    implemented pause audio

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
-const audioButton = document.querySelector('.aud-btton');
+function playAudio(){
+    audio.play();
+}
+
+function pauseAudio(){
+    audio.pause();
+}
+
+/* delete later */
+
+function testfunc(){
+    alert('you clicked this');
+}
+
+
+
+/* delete later */
+
+const playButton = document.querySelector('.aud-btton');
+const pauseButton = document.querySelector('.aud-btton');
 const audio = document.querySelector('#firetreasure');
 
-audioButton.addEventListener('click', function(){
-    audio.play();
-})
+playButton.addEventListener('click', function(){
+    if (audio.paused){
+        playAudio();
+    }
+
+    else{
+
+    pauseAudio();
+}
+});
+
+
+/* delete later 
+
+
+document.querySelector(".test1").addEventListener('click', function()){
+    testfunc();
+}
+
+
+/* delete later */
